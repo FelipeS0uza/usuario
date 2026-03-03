c181ee7a7b5cf5c251d928dba075ed66e576ebc9
FelipeSouza <felipesouza0500@gmail.com>
2026-03-03 20:02:53 -0300

    adicionados classes de security

---
+++ src/main/java/com/felipesouza/usuario/infrastructure/security/UserDetailsServiceImpl.java
@@ -0,0 +1,34 @@
+package com.felipesouza.usuario.infrastructure.security;
+
+
+import com.felipesouza.usuario.infrastructure.entity.Usuario;
+import com.felipesouza.usuario.infrastructure.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    // Repositório para acessar dados de usuário no banco de dados
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    // Implementação do metodo para carregar detalhes do usuário pelo e-mail
+    @Override
+    //throws é usada na assinatura de um metodo para declarar que ele pode lançar uma ou mais exceções
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        // Busca o usuário no banco de dados pelo e-mail
+        Usuario usuario = usuarioRepository.findByEmail(email)
+                //Caso não exista lança um erro com a mensangem de não encontrado
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado: " + email));
+
+        // Cria e retorna um objeto UserDetails com base no usuário encontrado
+        return org.springframework.security.core.userdetails.User
+                .withUsername(usuario.getEmail()) // Define o nome de usuário como o e-mail
+                .password(usuario.getSenha()) // Define a senha do usuário
+                .build(); // Constrói o objeto UserDetails
+    }
+}
